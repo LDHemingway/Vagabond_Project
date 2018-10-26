eba9bdd1b56fcc14107b24e03f441e01c0c6b0ed
miknhoj <kimjohan86@gmail.com>
2018-10-26 15:54:54 -0400

    added seed data for user, city and post

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+City.destroy_all
+User.destroy_all
+
+atlanta = City.create(location: "Atlanta", picture_url: "https://www.wheretraveler.com/sites/default/files/styles/promoted_image_social_large/public/skyline-atlanta_c-davidkosmossmith-flickr.jpg?itok=uMQTObyt" )
+
+bob = User.create(name: "Bob", current_city: "Atlanta")
+
+samplepost = Post.create(user_id: bob.id, city_id: atlanta.id, title: "CNN", comment: "This place dope.")

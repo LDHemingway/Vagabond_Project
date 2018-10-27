79ad267ed86d496bc80a90bc1c5431b7eb5c4ebd
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-10-27 11:56:00 -0400

    added cities controller

---
+++ public/precache-manifest.e6055e47e8f4d9d6d54505f6e8cc6272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81b06e19b048efe05d9a",
+    "url": "/static/js/main.81b06e19.chunk.js"
+  },
+  {
+    "revision": "8d8764e8c3d159a73ee2",
+    "url": "/static/js/1.8d8764e8.chunk.js"
+  },
+  {
+    "revision": "81b06e19b048efe05d9a",
+    "url": "/static/css/main.3fe16337.chunk.css"
+  },
+  {
+    "revision": "144d120645e5566ce26290e859e91f9d",
+    "url": "/index.html"
+  }
+];

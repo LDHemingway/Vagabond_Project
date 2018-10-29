8c8e1eabd2405ba9592e1a2e4b22c53d2549c4ba
elfein <elfein99@gmail.com>
2018-10-29 11:55:31 -0400

    delete on posts working

--- client/src/components/PostPage.jsx
+++ client/src/components/PostPage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import Axios from 'axios';
+import axios from 'axios';
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import NavBar from './shared_components/NavBar'
 
 const StyledDiv = styled.div`
@@ -32,22 +32,24 @@ export default class PostPage extends Component {
     city: {
       id: 4,
       location: ''
-    }
+    },
+    redirect: false,
+    showDelete: false
   }
 
   getPost = async () => {
     let post = { ...this.state.post }
-    const response = await Axios.get(`/api/cities/1/posts/${this.props.match.params.postId}`)
+    const response = await axios.get(`/api/cities/1/posts/${this.props.match.params.postId}`)
     post = response.data
     this.setState({ post })
   }
 
   getInfo = async () => {
     // let user = { ...this.state.user }
-    // const response = await Axios.get(`/api/users/1`)
+    // const response = await axios.get(`/api/users/1`)
     // user = response.data[0]
     let city = { ...this.state.city }
-    const cityResponse = await Axios.get(`/api/cities/${this.state.post.city_id}`)
+    const cityResponse = await axios.get(`/api/cities/${this.state.post.city_id}`)
     city = cityResponse.data[0]
     this.setState({ city })
   }
@@ -57,18 +59,42 @@ export default class PostPage extends Component {
     await this.getInfo()
   }
 
+  showDelete = () => {
+    this.setState({ showDelete: !this.state.showDelete })
+  }
+
+  deletePost = async () => {
+    await axios.delete(`/api/cities/${this.state.post.city_id}/posts/${this.state.post.id}`)
+    this.setState({ redirect: true })
+  }
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect to={`/cities/${this.state.post.city_id}`} />
+  }
+
     return (
       <StyledDiv>
         <NavBar title={this.state.post.title} />
         <div className='post-header'>
-          <Link to={`/users/${this.state.user.id}`} >
+          {/* <Link to={`/users/${this.state.user.id}`} >
             <img src={'https://thumbs.dreamstime.com/z/gl%C3%BCckliche-daumen-des-jungen-mannes-oben-lokalisiert-auf-wei%C3%9Fem-hintergrund-31653620.jpg'} alt='userpic' />
           </Link>
-          <h4>Author: <Link to={`/users/${this.state.user.id}`} >{this.state.user.name}</Link></h4>
+          <h4>Author: <Link to={`/users/${this.state.user.id}`} >{this.state.user.name}</Link></h4> */}
           <h4>City: <Link to={`/cities/${this.state.city.id}`} >{this.state.city.location}</Link></h4>
         </div>
         <p>{this.state.post.comment}</p>
+
+         {this.state.showDelete ?
+          <div>
+            <p>Are you sure you want to delete "{this.state.post.title}"?</p>
+            <button onClick={this.showDelete}>Cancel</button>
+            <p>or</p>
+            <button onClick={this.deletePost}>Delete Post :(</button>
+          </div>
+          :
+          <button id='delete' onClick={this.showDelete}>Delete</button>}
+
       </StyledDiv>
     )
   }

cc7404f034be30082e1fac66310dccdf8d160a58
elfein <elfein99@gmail.com>
2018-10-30 11:11:10 -0400

    updated styling on forms

--- client/src/components/EditPost.jsx
+++ client/src/components/EditPost.jsx
@@ -32,9 +32,10 @@ input {
 }
 textarea {
     height: 280px;
-    width: 100%;
+    width: 90%;
     max-width: 800px;
     font-size: 1.1em;
+    padding: 10px;
 }
 [type~=submit] {
   height: 50px;
@@ -56,6 +57,21 @@ h1 {
   font-size: 1.6em;
   color:  rgba(255,150,50, 0.8);
 }
+`
+const ColorDiv = styled.div`
+background-color: #fff;
+max-width: 800px;
+margin: 0 auto;
+height: 85vh;
+border-left: 1px solid rgba(60,190,180, 0.5);
+border-right: 1px solid rgba(60,190,180, 0.5);
+`
+
+const CoolDiv = styled.div`
+background-image: url('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/B2b8RSzfgivu3v9nb/modern-urban-sketch-city-center-with-skyscraper-building-cityscape-animated-available-in-4k-uhd-fullhd-and-hd-3d-video-animation-footage_sttrlm5ml_thumbnail-full15.png');
+background-size: contain;
+width: 100%;
+height: 85vh;
 `
 
 export default class EditPost extends Component {
@@ -101,39 +117,43 @@ export default class EditPost extends Component {
     return (
       <div>
         <NavBar />
-        <StyledForm onSubmit={this.handleSubmit} >
-          <h1>Update Post for {this.state.post.title}</h1>
-          <p>Title</p>
-          <input placeholder='Post Title'
-            type='text'
-            name='title'
-            value={this.state.updatedPost.title}
-            onChange={this.handleChange}
-          />
+        <CoolDiv>
+          <ColorDiv>
+            <StyledForm onSubmit={this.handleSubmit} >
+              <h1>Update Post for {this.state.post.title}</h1>
+              <p>Title</p>
+              <input placeholder='Post Title'
+                type='text'
+                name='title'
+                value={this.state.updatedPost.title}
+                onChange={this.handleChange}
+              />
 
-          <p>Image</p>
-          <input placeholder='Post Image Adress'
-            type='text'
-            name='photo_url'
-            value={this.state.updatedPost.photo_url}
-            onChange={this.handleChange}
-          />
+              <p>Image</p>
+              <input placeholder='Post Image Adress'
+                type='text'
+                name='photo_url'
+                value={this.state.updatedPost.photo_url}
+                onChange={this.handleChange}
+              />
 
-          <p>Content</p>
-          <textarea placeholder='Post Content'
-            name='content'
-            value={this.state.updatedPost.content}
-            onChange={this.handleChange}
-          />
+              <p>Content</p>
+              <textarea placeholder='Post Content'
+                name='content'
+                value={this.state.updatedPost.content}
+                onChange={this.handleChange}
+              />
 
-          <div>
-            <input type='submit' value='Update' />
-          </div>
+              <div>
+                <input type='submit' value='Update' />
+              </div>
 
-        </StyledForm>
-        <StyledDiv>
-          <Link to={`/cities/${this.props.match.params.cityId}`} ><button>Cancel</button></Link>
-        </StyledDiv>
+            </StyledForm>
+            <StyledDiv>
+              <Link to={`/cities/${this.props.match.params.cityId}`} ><button>Cancel</button></Link>
+            </StyledDiv>
+          </ColorDiv>
+        </CoolDiv>Î
       </div>
     )
   }
